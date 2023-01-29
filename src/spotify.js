90f4ceefc682abe46a4cbe08ff7f3b6b83fac102
MukundMaheswari178 <107140986+MukundMaheswari178@users.noreply.github.com>
2023-01-29 08:45:59 +0530

    Add files via upload

---
+++ src/spotify.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const clientId = "b498669b4f5541b18cf33663a39523fd";
+const redirectUri = "http://localhost:3000";
+const scopes = ["user-library-read", "playlist-read-private"];
+
+export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
+
+const apiClient = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+});
+
+export const setClientToken = (token) => {
+  apiClient.interceptors.request.use(async function (config) {
+    config.headers.Authorization = "Bearer " + token;
+    return config;
+  });
+};
+
+export default apiClient;
+
+
+
+
+
+
+
+
